0f130d6ef9c8c2b5712099bf94ad429121e2306e
Vulsan Bianca Maria <68532670+BiancaMariaVulsan@users.noreply.github.com>
2022-09-23 21:33:29 +0300

    add ingredient to shipping-list button works

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -15,5 +15,13 @@ export class ShoppingListService {
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
+    }    
+    
+    addIngredients(ingredients: Ingredient[]) {
+        // ingredients.forEach(element => {
+        //     this.ingredients.push(element);
+        // });
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
