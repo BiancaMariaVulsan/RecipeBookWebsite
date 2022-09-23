864ab7e291cfb46b8434330229c02857befa3aba
Vulsan Bianca Maria <68532670+BiancaMariaVulsan@users.noreply.github.com>
2022-09-23 12:52:27 +0300

    dropdown works

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropDownDirective {
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target);
+    }
+
+    // @HostListener('click') toggleOpen() {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+    constructor(private elRef: ElementRef) {}
+}
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: false}) ingredientName: ElementRef;
+  @ViewChild('amountInput', {static: false}) ingredientAmount: ElementRef;
+  @Output() addedIngredient = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddIngredient() {
+    this.addedIngredient.emit(new Ingredient(this.ingredientName.nativeElement.value, this.ingredientAmount.nativeElement.value));
   }
 
 }
